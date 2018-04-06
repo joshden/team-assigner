e316022efe3513a6c62b3746e884b5041eaca803
Josh Hayden <joshden@gmail.com>
2018-04-05 21:26:30 -0500

    Add missing teamBySpecialRequestOnly & queries
    Update index.ts to include required fullNameToFirstLastMapping.
    
    Also add commented out various queries in index.ts of log entries and
    team assignment results.

--- index.ts
+++ index.ts
@@ -6,14 +6,29 @@ import TeamAssigner from './src/TeamAssigner';
 import { AssignmentRuleMapping } from './src/AssignmentRuleMapping';
 
 // If this module cannot be found, start by copying customData.sample.ts to customData.ts and modifying it
-import { assignmentRuleMappings, teamsBySpecialRequestOnly } from './customData'; // TODO could also add to this corrections mappings like student last names
+import { assignmentRuleMappings, fullNameToFirstLastMapping, teamsBySpecialRequestOnly } from './customData'; // TODO could also add to this corrections mappings like student last names
 
 const logger = new Logger;
-const children = parseChildrenAndParents(path.resolve(__dirname, '..', '2018 FBA registration.xls'), logger);
+const children = parseChildrenAndParents(path.resolve(__dirname, '..', '2018 FBA registration.xls'), fullNameToFirstLastMapping, logger);
 const teams = parseTeamsAndTeachers(path.resolve(__dirname, '..', 'FBA Team Assignments 2018.xlsx'));
 const teamAssigner = new TeamAssigner;
 const assignedTeams = teamAssigner.assignTeams(children, teams, teamsBySpecialRequestOnly, assignmentRuleMappings, new Date('2018-04-13'), logger);
 
 // console.log(JSON.stringify(logger.entries, null, 2));
 // console.log(JSON.stringify(children, null, 2));
-// console.log(JSON.stringify(assignedTeams, null, 2));
+
+// logger.entries
+//     .filter(e => e.message.startsWith('Assuming child name'))
+//     .map(e => e.params[0] as any)
+//     .forEach(child => console.log(`Parent: ${child.$parents.FirstName} ${child.$parents.LastName}\nChild: ${child.ChildName}\n`));
+
+// const names = [
+//     'FirstName LastName'
+// ];
+// console.log(JSON.stringify(assignedTeams
+//     // .filter(t => t.team.teamNumber === 'A')
+//     .map(t => ({
+//         "team": t.team.teamNumber, 
+//         "children": t.children.map(c => `${c.firstName} ${c.lastName}`)}))
+//     .filter(t => t.children.some(name => names.includes(name)))
+//     , null, 2));
