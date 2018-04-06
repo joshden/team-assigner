72e9c63f759e3da7f552b4c4c6095a46a9e2dad1
Josh Hayden <joshden@gmail.com>
2018-04-05 19:16:27 -0500

    Add loger error level

--- src/Logger.ts
+++ src/Logger.ts
@@ -2,11 +2,19 @@ export default class Logger {
     readonly entries: {level: Level, message: string, params: any[]}[] = [];
 
     warning(message: string, ...params: any[]) {
-        this.entries.push({level: Level.warning, message: message, params: params});
+        this.pushEntry(Level.warning, message, params);
     }
 
+    error(message: string, ...params: any[]) {
+        this.pushEntry(Level.error, message, params);
+    }
+
+    private pushEntry(level: Level, message: string, params: any[]) {
+        this.entries.push({level, message, params});
+    }
 }
 
 enum Level {
-    warning
+    warning,
+    error
 }
