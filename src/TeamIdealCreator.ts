08408af5cfd565ef0357488dbac7c07d84aecdfc
Josh Hayden <joshden@gmail.com>
2018-04-07 00:32:09 -0500

    Refactor TeamIdealCreator with getDobRangeCounts
    Allow reuse in team scoring of calculting counts for groups

--- src/TeamIdealCreator.ts
+++ src/TeamIdealCreator.ts
@@ -1,19 +1,22 @@
 import { Team, IdealForTeam, GenderCounts, DateRangeCounts } from "./Team";
 import { ChildWithRules, Gender } from "./Child";
 
-export function createIdealsForTeams(assignableTeams: Team[], teamsBySpecialRequestOnly: Set<string>, allChildren: ChildWithRules[], maxGenderDobRangeCount = 3) {
+export type NullableDate = Date | null;
+
+export function createIdealsForTeams(assignableTeams: Team[], teamsBySpecialRequestOnly: Set<string>, allChildren: ChildWithRules[], maxGenderDobRangeCount = 4) {
     const specialRequestTeams = getSpecialRequestTeams(assignableTeams, teamsBySpecialRequestOnly);
     const nonSpecialTeamCount = assignableTeams.length - teamsBySpecialRequestOnly.size;
     const children = allChildren.filter(child => ! child.matchToUse || ! specialRequestTeams.has(child.matchToUse.team as Team));
     const childrenPerTeam = children.length / nonSpecialTeamCount;
     const minChildrenPerTeam = Math.floor(childrenPerTeam);
     const maxChildrenPerTeam = Math.ceil(childrenPerTeam);
-    const {byGender, byGenderDobRange, dobByGender} = createEmptyMaps();
+    const byGenderDobRange = new Map<Gender, Map<NullableDate, number>>();
+    const {byGender, dobByGender} = createEmptyMaps();
 
     children.forEach(child => {
         const gender = child.gender;
         byGender.set(gender, byGender.get(gender) as number + 1);
-        (dobByGender.get(gender) as Array<Date | null>).push(child.dateOfBirth);
+        (dobByGender.get(gender) as Array<NullableDate>).push(child.dateOfBirth);
     });
 
     for (const genderCount of Array.from(byGender.entries())) {
@@ -22,9 +25,8 @@ export function createIdealsForTeams(assignableTeams: Team[], teamsBySpecialRequ
 
     for (const entry of Array.from(dobByGender.entries())) {
         const [gender, allDobs] = entry;
-        const dobRangeCounts = (byGenderDobRange.get(gender) as Map<Date | null, number>)
         const dobs = allDobs.filter(dob => dob !== null) as Date[];
-        const rangeDates: Date[] = [];
+        const rangeDates: NullableDate[] = [null];
         dobs.sort((a, b) => a.getTime() - b.getTime());
         if (dobs.length > 0) {
             const oldest = dobs[0].getTime();
@@ -34,36 +36,49 @@ export function createIdealsForTeams(assignableTeams: Team[], teamsBySpecialRequ
                 rangeDates.push(new Date(oldest + sizeOfEachRange * range));
             }
         }
-        rangeDates.forEach(date => {
-            dobRangeCounts.set(date, 0);
-        });
-        dobs.forEach(dob => {
+
+        const dobRangeCounts = getDobRangeCounts(rangeDates, allDobs);
+        const dobRangeCountPerTeam = new Map(Array.from(dobRangeCounts.entries()).map(entry => [entry[0], entry[1] / nonSpecialTeamCount] as [NullableDate, number]));
+        byGenderDobRange.set(gender, dobRangeCountPerTeam);
+    }
+
+    // console.log(byGender);
+    // console.log(byGenderDobRange);
+
+    return assignableTeams.map(team => {
+        const isSpecialRequestTeam = specialRequestTeams.has(team);
+        return new IdealForTeam(
+            team,
+            isSpecialRequestTeam ? 0 : minChildrenPerTeam,
+            isSpecialRequestTeam ? 0 : maxChildrenPerTeam,
+            isSpecialRequestTeam ? createEmptyMaps().byGender : byGender,
+            isSpecialRequestTeam ? createEmptyMaps().byGenderDobRange : byGenderDobRange,
+            isSpecialRequestTeam)
+    });
+}
+
+
+export function getDobRangeCounts(rangeDates: NullableDate[], datesOfBirth: NullableDate[]) {
+    const dobRangeCounts = new Map<NullableDate, number>();
+
+    rangeDates.forEach(date => {
+        dobRangeCounts.set(date, 0);
+    });
+    
+    datesOfBirth.forEach(dob => {
+        let rangeToUse: NullableDate = null;
+        if (dob !== null) {
             for (const rangeDate of rangeDates) {
-                if (dob <= rangeDate) {
-                    dobRangeCounts.set(rangeDate, dobRangeCounts.get(rangeDate) as number + 1);
+                if (rangeDate !== null && dob <= rangeDate) {
+                    rangeToUse = rangeDate;
                     break;
                 }
             }
-        });
-        for (const rangeDate of rangeDates) {
-            dobRangeCounts.set(rangeDate, dobRangeCounts.get(rangeDate) as number / nonSpecialTeamCount);
         }
-        const nullDobCount = allDobs.length - dobs.length;
-        if (nullDobCount > 0) {
-            dobRangeCounts.set(null, nullDobCount / nonSpecialTeamCount);
-        }
-    }
-
-    return assignableTeams.map(team => {
-        const isSpecialRequestteam = specialRequestTeams.has(team);
-        return new IdealForTeam(
-            team,
-            isSpecialRequestteam ? 0 : minChildrenPerTeam,
-            isSpecialRequestteam ? 0 : maxChildrenPerTeam,
-            isSpecialRequestteam ? createEmptyMaps().byGender : byGender,
-            isSpecialRequestteam ? createEmptyMaps().byGenderDobRange : byGenderDobRange,
-            isSpecialRequestteam)
+        dobRangeCounts.set(rangeToUse, dobRangeCounts.get(rangeToUse) as number + 1);
     });
+    
+    return dobRangeCounts;
 }
 
 function getSpecialRequestTeams(allTeams: Team[], specialRequestNames: Set<string>) {
@@ -80,8 +95,8 @@ function getSpecialRequestTeams(allTeams: Team[], specialRequestNames: Set<strin
 
 function createEmptyMaps() {
     const byGender = new Map<Gender, number>();
-    const byGenderDobRange = new Map<Gender, Map<Date | null, number>>();
-    const dobByGender = new Map<Gender, Array<Date | null>>();
+    const byGenderDobRange = new Map<Gender, Map<NullableDate, number>>();
+    const dobByGender = new Map<Gender, Array<NullableDate>>();
 
     for (let item in Gender) {
         if (isNaN(Number(item))) {
